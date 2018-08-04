9e7bccb4287bdb83e927633ccc3ec7d6aa393c04
trashcan76 <41460747+trashcan76@users.noreply.github.com>
2018-08-04 12:51:50 -0500

    Read base64 PDF

--- src/Program.java
+++ src/Program.java
@@ -16,9 +16,10 @@ import name.fraser.neil.plaintext.diff_match_patch.Diff;
 
 public class Program {
 	
-    public static void main(String[] args) throws IOException {
-        String text1 = GetText("C:/Users/wzfis/Documents/Drums in Worship.pdf");
-        String text2 = GetText("C:/Users/wzfis/Documents/Drums in Worship 2.pdf");
+    public static void main(String[] args) throws IOException {  
+    	// get text from base64 encoded PDFs
+    	String text1 = GetText("C:\\Users\\wzfis\\eclipse-workspace\\HelloWorld\\pdfs\\drumsinworship.txt");
+        String text2 = GetText("C:\\Users\\wzfis\\eclipse-workspace\\HelloWorld\\pdfs\\drumsinworship2.txt");
                 
         // https://github.com/google/diff-match-patch/wiki/Language:-Java
         diff_match_patch dmp = new diff_match_patch();
@@ -35,12 +36,16 @@ public class Program {
     }
 
     public static String GetText(String path) throws IOException {
+    	// certutil -encode data.txt tmp.b64 && findstr /v /c:- tmp.b64 > data.b64certutil -encode data.txt tmp
+    	byte[] base64raw = Files.readAllBytes(Paths.get(path));    	
+    	byte[] base64decoded = java.util.Base64.getMimeDecoder().decode(base64raw);
+    	    
     	// https://www.programcreek.com/java-api-examples/?class=org.apache.pdfbox.pdmodel.PDDocument&method=load
-        PDDocument document = PDDocument.load(new File(path));
+    	PDDocument document = PDDocument.load(base64decoded);
         PDFTextStripper stripper = new PDFTextStripper();
         String documentText = stripper.getText(document);
         document.close();
 
         return documentText;
-    }
+    }    
 }
